349607039f32fbc2fc17dcd3fbf9ba3412bf117f
Daniel Mendonca <ei12167@fe.up.pt>
2015-12-29 23:13:19 +0000

    csoko_solver updated - now giving solution to more than one robot

--- csoko_thinker/src/csoko_solver.cpp
+++ csoko_thinker/src/csoko_solver.cpp
@@ -20,13 +20,15 @@ using namespace std;
 
 typedef vector<char> TableRow;
 typedef vector<TableRow> Table;
+typedef tuple<size_t, size_t> T_pos;
+typedef vector<T_pos> Vec_t_pos;
 
 struct Board {
 	Table sData, dData;
 	int px, py;
 
 
-	bool setRobot(int xx, int yy){
+	bool setRobot(size_t xx, size_t yy){
 		bool correctRobotPos = false;
 		if(sData.size()> yy && sData.at(yy).size() > xx)
 			if(sData.at(yy).at(xx) == '@')
@@ -190,7 +192,11 @@ struct Board {
 	}
 };
 
-void printBoard(Table t)
+
+/**
+ *
+ */
+void printBoard(const Table &t)
 {
 	for(auto row: t)
 	{
@@ -200,40 +206,245 @@ void printBoard(Table t)
 	}
 }
 
+
+/**
+ *Sets all robots, boxes and delivery points as walls
+ */
+void allAsWalls(Table &t, const Vec_t_pos &rs, const Vec_t_pos &bs, const Vec_t_pos &ds)
+{
+	for(T_pos r_pos : rs)
+	{
+		t.at(get<1>(r_pos)).at(get<0>(r_pos)) = '#';
+	}
+	for(T_pos b_pos : bs)
+	{
+		t.at(get<1>(b_pos)).at(get<0>(b_pos)) = '#';
+	}
+	for(T_pos d_pos : ds)
+	{
+		t.at(get<1>(d_pos)).at(get<0>(d_pos)) = '#';
+	}
+}
+
+
+/**
+ *
+ */
+void allAsBefore(Table &t, const Vec_t_pos &rs, const Vec_t_pos &bs, const Vec_t_pos &ds)
+{
+	for(T_pos r_pos : rs)
+	{
+		t.at(get<1>(r_pos)).at(get<0>(r_pos)) = '@';
+	}
+	for(T_pos b_pos : bs)
+	{
+		t.at(get<1>(b_pos)).at(get<0>(b_pos)) = '$';
+	}
+	for(T_pos d_pos : ds)
+	{
+		t.at(get<1>(d_pos)).at(get<0>(d_pos)) = '.';
+	}
+}
+
+
+/**
+ *Set a robot position, a box and a location to where the box should be taken in a Table
+ *@t table to add the elements
+ *@r robot position in the table
+ *@b box position in the table
+ *@d destiny position in the table where the box should be taken
+ */
+void setInterests(Table &t, const T_pos &r, const T_pos &b, const T_pos &d)
+{
+	t.at(get<1>(r)).at(get<0>(r)) = '@';
+	t.at(get<1>(b)).at(get<0>(b)) = '$';
+	t.at(get<1>(d)).at(get<0>(d)) = '.';
+}
+
+
+/**
+ *Sets a box and a delivery point in a Table as walls
+ */
+void negateInterest(Table &t, const T_pos &b, const T_pos &d)
+{
+	/*
+	cout<<"box x: " << get<0>(b)<<endl;
+	cout<<"box y: " << get<1>(b)<<endl;
+	cout<<"des x: " << get<0>(d)<<endl;
+	cout<<"des y: " << get<1>(d)<<endl;
+	 */
+	t.at(get<1>(b)).at(get<0>(b)) = '#';
+	t.at(get<1>(d)).at(get<0>(d)) = '#';
+}
+
+
+/**
+ *
+ */
+void performMove(Table &t, T_pos &r, T_pos &b, const string &sol, bool isPrint=false)
+{
+	int r_x = get<0>(r), r_y = get<1>(r);
+	int newX = 0, newY = 0;
+	for(auto c: sol)
+	{
+		if(c == 'd' || c == 'D')
+			newY = 1;
+		else if(c == 'u' || c == 'U')
+			newY= -1;
+		else newY=0;
+
+		if(c == 'l' || c == 'L')
+			newX= -1;
+		else if(c == 'r' || c == 'R')
+			newX = 1;
+		else newX=0;
+
+		r_y += newY;
+		r_x += newX;
+
+		if(t.at(r_y).at(r_x) == '$')
+			t.at(r_y +newY).at(r_x + newX) = '$';
+
+		t.at(r_y).at(r_x) = '@';
+		t.at(r_y).at(r_x) = ' ';
+
+		if(isPrint)
+			printBoard(t);
+	}
+	get<0>(r) = r_x;
+	get<1>(r) = r_y;
+	get<0>(b) = r_x + newX;
+	get<1>(b) = r_y + newY;
+	/*
+	cout<< "After performMove" <<endl;
+	cout<<"box x: " << get<0>(b)<<endl;
+	cout<<"box y: " << get<1>(b)<<endl;
+	cout<<"rob x: " << get<0>(r)<<endl;
+	cout<<"rob y: " << get<1>(r)<<endl;
+	cout <<endl<<endl;
+	 */
+}
+
+
+/**
+ *
+ */
+bool turn(const Table t, const Vec_t_pos rs, const Vec_t_pos bs, const Vec_t_pos ds, vector<vector<string> > &moves, size_t robot_nr)
+{
+	if(bs.size() == 0)
+		return true;
+
+	bool allSolved = false;
+
+	size_t robot_nr_now = robot_nr + 1;
+	if(robot_nr_now == rs.size()) robot_nr_now = 0;
+
+	size_t starting_robot = robot_nr_now;
+
+	do
+	{
+		for(size_t i = 0; i < bs.size(); i++)
+		{
+			for(size_t j = 0; j < ds.size(); i++)
+			{
+				Table t2(t);
+				Vec_t_pos rs2(rs);
+				Vec_t_pos bs2(bs);
+				Vec_t_pos ds2(ds);
+				size_t rx = get<0>(rs2.at(robot_nr_now)), ry = get<1>(rs2.at(robot_nr_now));
+				allAsWalls(t2, rs2, bs2, ds2);
+				setInterests(t2, rs2.at(robot_nr_now), bs2.at(i), ds2.at(j));
+
+				Board b(rx, ry, t2);
+				string sol = b.solve();
+
+				if(sol.at(0) == 'N')
+					continue; //can't solve
+
+				allAsBefore(t2, rs2, bs2, ds2);
+				/*
+				cout<< "Before performMove" <<endl;
+				cout<<"box x: " << get<0>(bs2.at(0))<<endl;
+				cout<<"box y: " << get<1>(bs2.at(0))<<endl;
+				cout<<"des x: " << get<0>(ds2.at(0))<<endl;
+				cout<<"des y: " << get<1>(ds2.at(0))<<endl;
+				cout <<endl<<endl;*/
+
+				performMove(t2, rs2.at(robot_nr_now), bs2.at(i), sol);
+				negateInterest(t2, bs2.at(i), ds2.at(j));
+
+				//printBoard(t2);
+
+				//save to restore
+				bs2.erase(bs2.begin() + i);
+				ds2.erase(ds2.begin() + j);
+				moves.at(robot_nr_now).push_back(sol);
+
+				allSolved = turn(t2, rs2, bs2, ds2, moves, robot_nr_now);
+
+				if(allSolved)
+					return true;
+
+				moves.at(robot_nr_now).pop_back();
+			}
+		}
+
+		robot_nr_now++;
+		robot_nr_now = (robot_nr_now == rs.size()) ? 0 : robot_nr_now;
+
+	} while( robot_nr_now != starting_robot);
+
+	return false;
+}
+
+
+/**
+ *
+ */
 void dynamicSolPrint(Table t){
 	printBoard(t);
-			Board b(0, 0, t);
-			int r_x = b.px;
-			int r_y = b.py;
-			string solution = b.solve();
+	Board b(0, 0, t);
+	int r_x = b.px;
+	int r_y = b.py;
+	string solution = b.solve();
 
-			for(auto c: solution){
-				int newX = 0, newY=0;
-				if(c == 'd' || c == 'D')
-					newY = 1;
-				else if(c == 'u' || c == 'U')
-					newY= -1;
-				else if(c == 'l' || c == 'L')
-					newX= -1;
-				else if(c == 'r' || c == 'R')
-					newX = 1;
+	if(solution.at(0) != 'N')
+		for(auto c: solution){
+			int newX = 0, newY=0;
+			if(c == 'd' || c == 'D')
+				newY = 1;
+			else if(c == 'u' || c == 'U')
+				newY= -1;
+			else if(c == 'l' || c == 'L')
+				newX= -1;
+			else if(c == 'r' || c == 'R')
+				newX = 1;
 
-				if(t.at(r_y +newY).at(r_x + newX) == '$')
-					t.at(r_y +newY + newY).at(r_x + newX + newX) = '$';
+			if(t.at(r_y +newY).at(r_x + newX) == '$')
+				t.at(r_y +newY + newY).at(r_x + newX + newX) = '$';
 
-				t.at(r_y +newY).at(r_x + newX) = '@';
-				t.at(r_y).at(r_x) = ' ';
-				r_y += newY;
-				r_x += newX;
+			t.at(r_y +newY).at(r_x + newX) = '@';
+			t.at(r_y).at(r_x) = ' ';
+			r_y += newY;
+			r_x += newX;
 
-				printBoard(t);
-			}
-			cout<<endl<<endl<<solution;
+			printBoard(t);
+		}
+	cout<<endl<<endl<<solution<<endl<<endl;
 }
-/*
+
+
+
+/**
+ *
+ *
 int main() {
 	Table t;
 	ifstream ifs("sokobanMap1");
+	vector<T_pos > rs; //robots
+	vector<T_pos > bs; //boxes
+	vector<T_pos > ds; //destinys
+
 	if(ifs.is_open())
 	{
 		string line;
@@ -243,16 +454,39 @@ int main() {
 			for(auto c : line)
 			{
 				row.push_back(c);
+				if(c == '@')
+					rs.push_back(make_tuple((row.size() -1),t.size()));
+				else if(c == '$')
+					bs.push_back(make_tuple((row.size() -1),t.size()));
+				else if(c == '.')
+					ds.push_back(make_tuple((row.size() -1),t.size()));
 			}
 			t.push_back(row);
 		}
 
-		ifs.close();
-		dynamicSolPrint(t);
+		cout << rs.size() << " " << bs.size() << " " << ds.size() << " " << endl << "rs: " << endl;
 
+
+		ifs.close();
+	}
+	vector<vector<string> > r_moves(rs.size());
+	bool solvable= turn(t, rs, bs, ds, r_moves, 0);
+
+
+	if(solvable)
+	{
+		for(string s : r_moves.at(0))
+		{
+			cout<<s<<endl;
+			//performMove(t, rs.at(0), bs.at(0), s, true);
+		}
+		for(string s : r_moves.at(1))
+		{
+			cout<<s<<endl;
+			//performMove(t, rs.at(0), bs.at(0), s, true);
+		}
 	}
 
 	return 0;
 }
 */
-
